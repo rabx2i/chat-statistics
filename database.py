1c2edac95abaf9d4e4600546dee6acd91c83e484
rabx2i <rabx2i@protonmail.com>
2020-05-12 21:08:09 +0300

    Version 0.0.2

---
+++ database.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+def ensure_connection(func):
+	def inner(*args, **kwargs):
+		with sqlite3.connect("database.db") as conn:
+			res = func(conn, *args, **kwargs)
+		return res
+	return inner
+
+@ensure_connection
+def check_db(conn, force: bool = False):
+	c = conn.cursor()
+	if force:
+		c.execute("DROP TABLE IF EXISTS user_language")
+	c.execute("""
+		CREATE TABLE IF NOT EXISTS user_language (
+			id           INTEGER PRIMARY KEY,
+			user_id      INTEGER NOT NULL,
+			language     TEXT NOT NULL
+		)
+	""")
+	conn.commit()
+
+@ensure_connection
+def choice_language(conn, user_id: int, language: str):
+	c = conn.cursor()
+	c.execute("INSERT INTO user_language (user_id, language) VALUES (?, ?)", (user_id, language))
+	conn.commit()
